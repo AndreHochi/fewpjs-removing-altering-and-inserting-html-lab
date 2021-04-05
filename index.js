f14f1c6cfbb4bcaf5c6bb0e2226ffd4074a17c7f
Andrehochi <andrehochiwork@gmail.com>
2021-04-05 09:45:40 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main = document.getElementById("main");
+main.remove();
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+let text = document.createTextNode("YOUR-NAME is the champion!")
+newHeader.appendChild(text);
